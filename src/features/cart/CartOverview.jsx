dde1c0d87dc0daa8d4d298822f2c2c8ecce172a8
aleksandrawozniak965 <aleksandra.wozniak965@gmail.com>
2024-08-16 13:17:30 +0200

    tailwind grid&flex styling

--- src/features/cart/CartOverview.jsx
+++ src/features/cart/CartOverview.jsx
@@ -2,8 +2,8 @@ import { Link } from "react-router-dom";
 
 function CartOverview() {
   return (
-    <div className="bg-stone-800 text-stone-200 uppercase" >
-      <p className="text-stone-300 font-semibold">
+    <div className="flex items-center justify-between bg-stone-800 text-stone-200 text-sm uppercase px-4 py-4 sm:px-6 md:text-base" >
+      <p className="space-x-4 sm:space-x-6 text-stone-300 font-semibold">
         <span>23 pizzas</span>
         <span>$23.45</span>
       </p>
